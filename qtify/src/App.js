15365df0560b1fe25d35a4e09b3c8dd7b27a9f0f
sarvagya-code <sarvagyasrivastava8@gmail.com>
2024-05-09 12:49:01 +0530

    songs implemented

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -5,12 +5,21 @@ import styles from './App.module.css'
 import { useEffect, useState } from 'react'
 import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from './api/api'
 import Section from './components/Section/Section.jsx'
+import FilterSection from './components/FilterSection/FilterSection.jsx'
 
 function App() {
 
-  const [topAlbumSongs, setTopAlbumSongs] = useState([])
-  const [newAlbumSongs, setNewAlbumSongs] = useState([])
+  const [topAlbumSongs, setTopAlbumSongs] = useState([]);
+  const [newAlbumSongs, setNewAlbumSongs] = useState([]);
   
+  //state to store  original array of songs(unfiltered)
+  const[songsData, setSongsData]=useState([]);
+
+  //to store the index selected inn filters, can be ignored
+  const[value,setValue]= useState(0);
+ 
+  //to store the finally filtered songs
+  const[filteredData, setFilteredData]=useState([]);
 
   const generateTopAlbumSongs = async () => {
     try {
@@ -34,12 +43,65 @@ function App() {
     }
   }
 
-  useEffect(() => {
+  const generateSongs = async() => {
+    try {
+      console.log("generateSongs");
+      const res=await fetchSongs();
+      setSongsData(res);
+      setFilteredData(res);
+    }
+    catch(error) {
+      return null;
+    }
+  }
+
+  //function to generate filtered songs after selecting one tab
+  const generateNewSongs=(index)=>{
+
+    let key="";
+    if(index===0){
+      // suppose someOne select 0th tab after 2nd tab 
+      //set the default songsData as the final filtered data, bcz we need to show all of songs now
+      generateSongs();
+      return;
+    }
+    else if(index===1){
+      key="rock";
+    }
+    else if(index===2){
+      key="pop";
+    }
+
+    else if(index===3){
+      key="jazz";
+    }
+    else if(index===4){
+      key="blues";
+    }
+
+    let newSongsArray=songsData.filter((song)=>{
+      console.log("key: ",key)
+      return(song.genre.key===key);
+    })
+
+    console.log("generateNewSongs triggered and filtered this Data: ", newSongsArray)
+    setFilteredData(newSongsArray);
+  }
+  //rock pop jazz blues
+  //to handle any change in the selected tab of the songs section and call the generateNewSongs
+  const handleChangeIndex= async(newValue)=>{
+    console.log("handleChangeIndex triggered with newValue: ",newValue)
+    setValue(newValue);
+    generateNewSongs(newValue);
+  }
+
+    useEffect(() => {
+      
+      generateTopAlbumSongs();
+      generateNewAlbumSongs();
+      generateSongs();
+    }, [])
     
-    generateTopAlbumSongs();
-    generateNewAlbumSongs();
-  }, [])
-  
   
   return (
     <>
@@ -48,6 +110,8 @@ function App() {
       <div className={styles.sectionWrapper}>
         <Section type='album' title='Top Albums' data={topAlbumSongs} />
         <Section type='album' title='New Albums' data={newAlbumSongs} />
+        <FilterSection  type='song' title='Songs' value={value} filteredData={filteredData} handleChangeIndex={handleChangeIndex}/>
+
       </div>
     </>
   )
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,40 +1,54 @@
-import React, { useState } from "react";
-import styles from "./Section.module.css";
+import React, { useState } from 'react'
+import { CircularProgress } from '@mui/material';
 import Card from "../Card/Card";
-import { CircularProgress } from "@mui/material";
-import Carousel from "../Carousel/Carousel";
+import styles from "./Section.module.css"
+import Carousel from '../Carousel/Carousel';
 
-const Section = ({ title, data, type }) => {
-  const [carouselToggle, setCarouselToggle] = useState(true);
+const Section=({type,title,data,toggle=true})=> {
 
-  const handleToggle = () => {
-    setCarouselToggle(!carouselToggle);
-  };
+    const[carouselToggle,setCarouselToggle]=useState(true);
+
+    const handleToggle=()=>{
+        setCarouselToggle(!carouselToggle);
+    }
+
   return (
     <div>
-      <div className={styles.header}>
-        <h3>{title}</h3>
-        <h4 className={styles.toggleText} onClick={handleToggle}>
-          {carouselToggle ? "Show All" : "Collapse All"}
-        </h4>
-      </div>
-      {data.length === 0 ? (
-        <CircularProgress />
-      ) : (
-        <div className={styles.cardWrapper}>
-          {!carouselToggle ? (
-            <div className={styles.wrapper}>
-                {data.map((card) => (
-                <Card data={card} type={type} key={card.id} />
-                ))}
-            </div>
-          ) : (
-            <Carousel data={data} renderCardComponent={(data) => <Card data={data} type={type}/>}/>
-          )}
+        <div className={styles.sectionTop}>
+            <h3>{title}</h3>
+            <h4 onClick={handleToggle} className={styles.toggleText}>
+
+            {toggle?(
+                 carouselToggle?"Show All":"Collapse All"
+            ):(
+                <></>
+            )}
+            </h4>
         </div>
-      )}
+        
+        {data.length?(
+            <div className={styles.sectionInnerWrapper}>
+            {!carouselToggle?(
+                <div className={styles.showAllWrapper}>
+                {data.map((album)=>(
+                    <Card data={album} type={type} key={album.id}/>
+                ))}
+                </div>
+            ):(
+              <div>
+              <Carousel data={data} renderCardComponent={(data)=><Card data={data} type={type}/>}/>
+              </div>  
+            )}
+            </div>
+        ):(
+            <div className={styles.progressBar}>
+            <CircularProgress />
+            </div>
+        )}
+
     </div>
-  );
-};
+  )
+}
 
 export default Section;
+
